c00ff24666c4b63f1b078ca1a4dce62e92dbc4cc
Nicole Yang <nicoleyry@outlook.com>
2023-07-11 22:43:01 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<Table />
+</template>
+
+<script>
+import Table from './components/Table.vue';
+
+export default {
+	name: 'App',
+	components: {
+		Table,
+	}
+}
+</script>
+
+<style lang="scss">
+	* {
+		margin: 0;
+		padding: 0;
+	}
+	html {
+		background-color: $bg-color;
+	}
+	html::-webkit-scrollbar,
+	body::-webkit-scrollbar {
+		display: none;
+	}
+	#app {
+		font-family: "Helvetica Neue", "Helvetica", "Arial", "PingFang TC", "PingFang SC", "Microsoft JhengHei", "Microsoft YaHei", "sans-serif";
+		text-align: center;
+		color: $text-color-default;
+		height: 100vh;
+	}
+</style>
